793166428dea98cdb1c68295dd39bfee6147990b
Thomas Wood <woodthom@gmail.com>
2023-05-05 22:09:44 +0100

    Redeploy

--- deployment/matcher_lambda/function/lambda_function.py
+++ deployment/matcher_lambda/function/lambda_function.py
@@ -1,5 +1,7 @@
 import json
 import os
+import pickle as pkl
+from collections import Counter
 
 import numpy as np
 import requests
@@ -40,13 +42,72 @@ except:
 
 print("Loaded embeddings", len(mhc_embeddings))
 
+CACHE_FILE = "/tmp/harmony_vectors_cache.pkl"
+
+item_to_vectors_cache = {}
+invocation_counter = Counter()
+
+if os.path.isfile(CACHE_FILE):
+    try:
+        with open(CACHE_FILE, "rb") as f:
+            item_to_vectors_cache = pkl.load(f)
+    except:
+        print("Could not load cache")
+
 
 def vectorisation_function(all_texts: np.ndarray) -> np.ndarray:
-    response = requests.post("https://uroe37564sqhuuczvqitupuv440zcdgf.lambda-url.eu-west-2.on.aws",
-                             headers=headers,
-                             json={"texts": all_texts})
-    all_vectors = np.asarray(response.json())
-    return all_vectors
+    # Work out which texts aren't in the cache.
+    indices_to_calculate = []
+    calculation_lookup = {}
+    for idx, text in enumerate(all_texts):
+        if text not in item_to_vectors_cache:
+            calculation_lookup[idx] = len(indices_to_calculate)
+            indices_to_calculate.append(idx)
+
+    # If there are any non-cached items, we call the neural network.
+    if len(indices_to_calculate) > 0:
+        if len(indices_to_calculate) == len(all_texts):
+            texts_to_send_to_neural_network = all_texts
+        else:
+            texts_to_send_to_neural_network = all_texts[indices_to_calculate, :]
+
+        response = requests.post("https://uroe37564sqhuuczvqitupuv440zcdgf.lambda-url.eu-west-2.on.aws",
+                                 headers=headers,
+                                 json={"texts": texts_to_send_to_neural_network})
+
+        response_vectors = np.asarray(response.json())
+
+        for idx, text in enumerate(texts_to_send_to_neural_network):
+            item_to_vectors_cache[text] = response_vectors[idx, :]
+
+        invocation_counter[0] += 1
+        if invocation_counter[0] % 5 == 0:
+            try:
+                with open(CACHE_FILE, "wb") as f:
+                    pkl.dump(item_to_vectors_cache, f)
+            except:
+                print("Could not save cache")
+
+        # all items came from API
+        if len(indices_to_calculate) == len(all_texts):
+            return response_vectors
+
+        items = []
+        for idx, text in enumerate(all_texts):
+            if idx in calculation_lookup:
+                items.append(response_vectors[calculation_lookup, :])
+            else:
+                items.append(item_to_vectors_cache[text])
+
+        return np.asarray(items, dtype=np.float)
+
+    else:
+        # all items came from cache
+        items = []
+        for text in all_texts:
+            items.append(item_to_vectors_cache[text])
+
+        return np.asarray(items, dtype=np.float)
 
 
 def lambda_handler(event, context):
