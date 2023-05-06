405137f2422767b689693d5fd2188991351c3a16
Thomas Wood <woodthom@gmail.com>
2023-05-07 00:49:22 +0100

    Delete deployment/old directory

--- deployment/old/text_to_instruments_lambda_python/0-run-tests.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-set -eo pipefail
-python3 function/test_lambda_function.py
--- deployment/old/text_to_instruments_lambda_python/2-build-layer.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -eo pipefail
-rm -rf package
-cd function
-pip3 install --target ../package/python -r requirements.txt
-# pip3 install --no-dependencies --target ../package/python -e ../../harmony_pypi_package
-cd ..
-cp -r ../../harmony_pypi_package/build/lib/harmony/ package/python/
