d5a80abde42dc6c33a8e869fe8d1808efa176103
Thomas Wood <woodthom@gmail.com>
2023-05-18 22:31:41 +0100

    SAM build

--- deployment/pdf_processor_tika_lambda_java/src/test/java/harmony/TestLogger.java
+++ deployment/pdf_processor_tika_lambda_java/src/test/java/harmony/TestLogger.java
@@ -4,7 +4,6 @@ import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import java.io.*;
 
 public class TestLogger implements LambdaLogger {
-  private static final Logger logger = LoggerFactory.getLogger(TestLogger.class);
   public TestLogger(){}
   public void log(String message){
     System.out.println(message);
