969e2e3f039c6fae87dae498f086d1a0a239da28
Thomas Wood <woodthom@gmail.com>
2023-05-19 21:04:33 +0100

    update

--- deployment/terraform/security.tf
+++ deployment/terraform/security.tf
@@ -3,13 +3,13 @@ resource "aws_security_group" "ec2" {
   description = "Allow efs outbound traffic"
   vpc_id      = aws_vpc.vpc.id
   ingress {
-from_port        = 0
+    from_port        = 0
     to_port          = 0
     protocol         = "-1"
     cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
   }
- egress {
+  egress {
     from_port        = 0
     to_port          = 0
     protocol         = "-1"
@@ -27,14 +27,14 @@ resource "aws_security_group" "efs" {
   vpc_id      = aws_vpc.vpc.id
 
   ingress {
-from_port        = 0
+    from_port        = 0
     to_port          = 0
     protocol         = "-1"
     cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
   }
 
- egress {
+  egress {
     from_port        = 0
     to_port          = 0
     protocol         = "-1"
