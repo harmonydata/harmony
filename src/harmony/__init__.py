18a6d00ad84422b157ddc86cde33469120890c87
Thomas Wood <woodthom@gmail.com>
2023-06-10 22:53:14 +0100

    exclude transformers

--- src/harmony/__init__.py
+++ src/harmony/__init__.py
@@ -1,6 +1,7 @@
 __version__ = "0.1.0"
 
 # TODO: make these configurable at package level
+import os
 from .parsing.text_parser import convert_text_to_instruments
 from .parsing.excel_parser import convert_excel_to_instruments
 from .parsing.pdf_parser import convert_pdf_to_instruments
@@ -8,5 +9,6 @@ from .parsing.wrapper_all_parsers import convert_files_to_instruments
 from .parsing import *
 from .schemas import *
 from .matching.matcher import match_instruments_with_function
-from .matching.default_matcher import match_instruments
+if "no_transformers" not in os.environ.get("HARMONY_LITE", ""):
+    from .matching.default_matcher import match_instruments
 from .util.file_helper import load_instruments_from_local_file
