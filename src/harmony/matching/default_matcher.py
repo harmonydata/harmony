c51c89a05cc0231f0bd5c7a4b1cf28e9dc4d3816
Thomas Wood <woodthom@gmail.com>
2023-06-24 22:15:30 +0100

    update environment vars

--- src/harmony/matching/default_matcher.py
+++ src/harmony/matching/default_matcher.py
@@ -8,10 +8,12 @@ from harmony import match_instruments_with_function
 from harmony.schemas.requests.text import Instrument
 import os
 
-if os.environ.get("HARMONY_DATA_PATH", None) is not None:
-    model = SentenceTransformer(os.environ["HARMONY_DATA_PATH"] + '/paraphrase-multilingual-MiniLM-L12-v2')
+if os.environ.get("HARMONY_SENTENCE_TRANSFORMER_PATH", None) is not None and os.environ.get("HARMONY_SENTENCE_TRANSFORMER_PATH", None) != "":
+    sentence_transformer_path = os.environ["HARMONY_SENTENCE_TRANSFORMER_PATH"]
 else:
-    model = SentenceTransformer("sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2")
+    sentence_transformer_path = "sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2"
+
+model = SentenceTransformer(sentence_transformer_path)
 
 def convert_texts_to_vector(texts: np.ndarray):
     embeddings = model.encode(texts)
--- src/harmony/parsing/wrapper_all_parsers.py
+++ src/harmony/parsing/wrapper_all_parsers.py
@@ -1,25 +1,14 @@
 import os
 from typing import List
 
-import pyfscache
 from harmony.parsing.excel_parser import convert_excel_to_instruments
 from harmony.parsing.pdf_parser import convert_pdf_to_instruments
 from harmony.parsing.text_parser import convert_text_to_instruments
 from harmony.schemas.enums.file_types import FileType
 from harmony.schemas.requests.text import RawFile, Instrument
-from flask_caching import Cache
-from main import app
-
-cache_path = os.getenv("HARMONY_DATA_PATH", ".") + "/parse_cache"
-cache = Cache(app, config={
-    'CACHE_TYPE': 'filesystem',
-    'CACHE_DIR': cache_path,
-    'CACHE_DEFAULT_TIMEOUT': 3600 * 24 * 30,
-})
 
 
-@cache.memoize()
-def cached_parse_file(file):
+def _get_instruments_from_file(file):
     if file.file_type == FileType.pdf:
         instruments_from_this_file = convert_pdf_to_instruments(file)
     elif file.file_type == FileType.txt:
@@ -30,12 +19,11 @@ def cached_parse_file(file):
         instruments_from_this_file = []
     return instruments_from_this_file
 
-
 def convert_files_to_instruments(files: List[RawFile]) -> List[Instrument]:
     instruments = []
 
     for file in files:
-        instruments_from_this_file = cached_parse_file(file)
+        instruments_from_this_file = _get_instruments_from_file(file)
         instruments.extend(instruments_from_this_file)
 
     return instruments
