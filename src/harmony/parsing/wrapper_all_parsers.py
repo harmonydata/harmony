2efab1fd4f986dbb21a6a5d2d763c0f3c8de7dd4
Zairon Jacobs <zaijacobs@gmail.com>
2023-07-07 12:32:19 +0200

    undo cache in convert_files_to_instruments

--- src/harmony/parsing/wrapper_all_parsers.py
+++ src/harmony/parsing/wrapper_all_parsers.py
@@ -5,8 +5,6 @@ from harmony.parsing.pdf_parser import convert_pdf_to_instruments
 from harmony.parsing.text_parser import convert_text_to_instruments
 from harmony.schemas.enums.file_types import FileType
 from harmony.schemas.requests.text import RawFile, Instrument
-from harmony.services.instruments_cache import InstrumentsCache
-from harmony.util import cache_heper
 
 
 def _get_instruments_from_file(file):
@@ -24,27 +22,10 @@ def _get_instruments_from_file(file):
 def convert_files_to_instruments(files: List[RawFile]) -> List[Instrument]:
     """Get cached instruments of files or convert files to instruments"""
 
-    instruments_cache = InstrumentsCache()
-
-    instruments: List[Instrument] = []
-
-    # A list of files whose instruments are not cached
-    files_with_no_cached_instruments = []
+    instruments = []
 
     for file in files:
-        hash_value = cache_heper.get_hash_value(file.content)
-        if instruments_cache.has(hash_value):
-            # If instruments are cached
-            instruments.extend(instruments_cache.get(hash_value))
-        else:
-            # If instruments are not cached
-            files_with_no_cached_instruments.append(file)
-
-    # Get instruments that aren't cached yet and cache them
-    for file_with_no_cached_instruments in files_with_no_cached_instruments:
-        new_instruments = _get_instruments_from_file(file_with_no_cached_instruments)
-        hash_value = cache_heper.get_hash_value(file_with_no_cached_instruments.content)
-        instruments_cache.set(hash_value, new_instruments)
-        instruments.extend(new_instruments)
+        instruments_from_this_file = _get_instruments_from_file(file)
+        instruments.extend(instruments_from_this_file)
 
     return instruments
--- src/harmony/util/cache_heper.py
+++
@@ -1,7 +0,0 @@
-from hashlib import sha256
-
-
-def get_hash_value(text: str) -> str:
-    """Get hash value"""
-
-    return sha256(text.encode()).hexdigest()
