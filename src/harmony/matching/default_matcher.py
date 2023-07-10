b3d8002aa3058e00571e16ff469209005858e139
Zairon Jacobs <zaijacobs@gmail.com>
2023-07-10 17:39:14 +0200

    check cached vectors in match_instruments_with_function

--- src/harmony/matching/default_matcher.py
+++ src/harmony/matching/default_matcher.py
@@ -1,27 +1,46 @@
+import os
 from typing import List
 
 import numpy as np
-
+from numpy import ndarray
 from sentence_transformers import SentenceTransformer
 
 from harmony import match_instruments_with_function
 from harmony.schemas.requests.text import Instrument
-import os
 
-if os.environ.get("HARMONY_SENTENCE_TRANSFORMER_PATH", None) is not None and os.environ.get("HARMONY_SENTENCE_TRANSFORMER_PATH", None) != "":
+if (
+    os.environ.get("HARMONY_SENTENCE_TRANSFORMER_PATH", None) is not None
+    and os.environ.get("HARMONY_SENTENCE_TRANSFORMER_PATH", None) != ""
+):
     sentence_transformer_path = os.environ["HARMONY_SENTENCE_TRANSFORMER_PATH"]
 else:
-    sentence_transformer_path = "sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2"
+    sentence_transformer_path = (
+        "sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2"
+    )
 
 model = SentenceTransformer(sentence_transformer_path)
 
-def convert_texts_to_vector(texts: np.ndarray):
-    embeddings = model.encode(texts)
+
+def convert_texts_to_vector(texts: List) -> ndarray:
+    embeddings = model.encode(sentences=texts, convert_to_numpy=True)
+
     return embeddings
 
 
-def match_instruments(instruments: List[Instrument], query: str = None, mhc_questions: List = [],
-                      mhc_all_metadatas: List = [],
-                      mhc_embeddings: np.ndarray = np.zeros((0, 0))) -> tuple:
-    return match_instruments_with_function(instruments, query, convert_texts_to_vector, mhc_questions,
-                                           mhc_all_metadatas, mhc_embeddings)
+def match_instruments(
+    instruments: List[Instrument],
+    query: str = None,
+    mhc_questions: List = [],
+    mhc_all_metadatas: List = [],
+    mhc_embeddings: np.ndarray = np.zeros((0, 0)),
+    texts_cached_vectors: dict[str, List[float]] = {},
+) -> tuple:
+    return match_instruments_with_function(
+        instruments=instruments,
+        query=query,
+        vectorisation_function=convert_texts_to_vector,
+        mhc_questions=mhc_questions,
+        mhc_all_metadatas=mhc_all_metadatas,
+        mhc_embeddings=mhc_embeddings,
+        texts_cached_vectors=texts_cached_vectors,
+    )
--- src/harmony/matching/matcher.py
+++ src/harmony/matching/matcher.py
@@ -1,48 +1,101 @@
-import types
 from collections import Counter
-from typing import List
+from typing import List, Callable
 
 import numpy as np
 from numpy import dot, mat, matmul, ndarray
 from numpy.linalg import norm
 
-from harmony.schemas.requests.text import Instrument
 from harmony.matching.negator import negate
+from harmony.schemas.requests.text import Instrument
+
 
 def cosine_similarity(vec1: ndarray, vec2: ndarray) -> ndarray:
     dp = dot(vec1, vec2.T)
     m1 = mat(norm(vec1, axis=1))
     m2 = mat(norm(vec2.T, axis=0))
+
     return np.asarray(dp / matmul(m1.T, m2))
 
 
-def match_instruments_with_function(instruments: List[Instrument], query: str,
-                                    vectorisation_function: types.FunctionType, mhc_questions: List = [],
-                                    mhc_all_metadatas: List = [],
-                                    mhc_embeddings: np.ndarray = np.zeros((0, 0))) -> tuple:
+def match_instruments_with_function(
+    instruments: List[Instrument],
+    query: str,
+    vectorisation_function: Callable,
+    mhc_questions: List = [],
+    mhc_all_metadatas: List = [],
+    mhc_embeddings: np.ndarray = np.zeros((0, 0)),
+    texts_cached_vectors: dict[str, List[float]] = {},
+) -> tuple:
+    """
+    Match instruments
+
+    :param instruments: The instruments
+    :param query: The query
+    :param vectorisation_function: A function to vectorize a text
+    :param mhc_questions
+    :param mhc_all_metadatas
+    :param mhc_embeddings
+    :param texts_cached_vectors: A dictionary of already cached vectors from texts (key is the text and value is the vector)
+    """
+
+    # Vectors of texts that are already cached
+    vectors_pos_cached = []
+    vectors_neg_cached = []
+
     texts = []
     negated_texts = []
-    instrument_ids = []
-    question_indices = []
     all_questions = []
     for instrument in instruments:
         for question_idx, question in enumerate(instrument.questions):
             question.instrument_id = instrument.instrument_id
             all_questions.append(question)
-            texts.append(question.question_text)
-            negated = negate(question.question_text, instrument.language)
-            negated_texts.append(negated)
-            instrument_ids.append(instrument.instrument_id)
-            question_indices.append(question_idx)
 
+            # Text
+            question_text = question.question_text
+            if question_text not in texts_cached_vectors.keys():
+                texts.append(question_text)
+            else:
+                vectors_pos_cached.append(texts_cached_vectors[question_text])
+
+            # Negated text
+            negated_text = negate(question_text, instrument.language)
+            if negated_text not in texts_cached_vectors.keys():
+                negated_texts.append(negated_text)
+            else:
+                vectors_neg_cached.append(texts_cached_vectors[negated_text])
+
+    # Add all texts together including query
     all_texts = texts + negated_texts
     if query:
         all_texts.append(query)
 
+    # Get vectors for all texts
     all_vectors = vectorisation_function(all_texts)
 
-    vectors_pos = all_vectors[:len(texts), :]
-    vectors_neg = all_vectors[len(texts):len(texts) * 2, :]
+    # Create a dictionary with new vectors
+    new_vectors = {}
+    for vector, text in zip(all_vectors, all_texts):
+        new_vectors[text] = vector
+
+    # Create numpy array of texts vectors
+    tmp_vectors_pos = all_vectors[: len(texts), :]
+    if vectors_pos_cached:
+        # Concatenate the new vectors and the cached vectors
+        vectors_pos = np.concatenate([tmp_vectors_pos, vectors_pos_cached], axis=0)
+    else:
+        # New vectors only
+        vectors_pos = tmp_vectors_pos
+
+    # Create numpy array of negated texts vectors
+    tmp_vectors_neg = all_vectors[len(texts) : len(texts) * 2, :]
+    if vectors_neg_cached:
+        # Concatenate the new vectors and the cached vectors
+        vectors_neg = np.concatenate([tmp_vectors_neg, vectors_neg_cached], axis=0)
+    else:
+        # New vectors only
+        vectors_neg = tmp_vectors_neg
+
+    # Get query similarity
     if query:
         vector_query = all_vectors[-1:, :]
         query_similarity = cosine_similarity(vectors_pos, vector_query)[:, 0]
@@ -52,10 +105,13 @@ def match_instruments_with_function(instruments: List[Instrument], query: str,
     pairwise_similarity = cosine_similarity(vectors_pos, vectors_pos)
     pairwise_similarity_neg1 = cosine_similarity(vectors_neg, vectors_pos)
     pairwise_similarity_neg2 = cosine_similarity(vectors_pos, vectors_neg)
-    pairwise_similarity_neg_mean = np.mean([pairwise_similarity_neg1, pairwise_similarity_neg2], axis=0)
+    pairwise_similarity_neg_mean = np.mean(
+        [pairwise_similarity_neg1, pairwise_similarity_neg2], axis=0
+    )
 
     similarity_difference = pairwise_similarity - pairwise_similarity_neg_mean
     similarity_polarity = np.sign(similarity_difference)
+
     # Make sure that any 0's in polarity are converted to 1's
     where_0 = np.where(np.abs(similarity_difference) < 0.001)
     similarity_polarity[where_0] = 1
@@ -86,4 +142,4 @@ def match_instruments_with_function(instruments: List[Instrument], query: str,
         for question in all_questions:
             question.topics_auto = instrument_to_category[question.instrument_id]
 
-    return all_questions, similarity_with_polarity, query_similarity
+    return all_questions, similarity_with_polarity, query_similarity, new_vectors
