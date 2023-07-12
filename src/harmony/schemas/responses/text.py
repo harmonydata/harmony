413e286fdda75243ab72adc87842957be55e747e
Zairon Jacobs <zaijacobs@gmail.com>
2023-07-12 07:57:12 +0200

    add cache response schema

--- src/harmony/schemas/responses/text.py
+++ src/harmony/schemas/responses/text.py
@@ -1,15 +1,24 @@
 from typing import List
 
+from harmony.schemas.requests.text import Instrument
+from harmony.schemas.requests.text import Question
 from pydantic import BaseModel, Field
-
-from harmony.schemas.requests.text import Question, Instrument
 
 
 class MatchResponse(BaseModel):
     questions: List[Question] = Field(
-        description='The questions which were matched, in an order matching the order of the matrix')
-    matches: List[List] = Field(description='Matrix of cosine similarity matches')
-    query_similarity: List = Field(None, description='Similarity metric between query string and items')
-    
+        description="The questions which were matched, in an order matching the order of the matrix"
+    )
+    matches: List[List] = Field(description="Matrix of cosine similarity matches")
+    query_similarity: List = Field(
+        None, description="Similarity metric between query string and items"
+    )
+
+
 class InstrumentList(BaseModel):
     __root__: List[Instrument]
+
+
+class CacheResponse(BaseModel):
+    instruments: List[Instrument] = Field(description="A list of instruments")
+    vectors: List[dict] = Field(description="A list of vectors")
