eb5c0c010e2bdd4de998f93a1635b2976c3632ff
Thomas Wood <woodthom@gmail.com>
2023-07-13 09:50:28 +0100

    comment and document

--- src/harmony/util/file_helper.py
+++ src/harmony/util/file_helper.py
@@ -31,13 +31,13 @@ def load_instruments_from_local_file(file_name: str) -> List[Instrument]:
         file_as_base64 = base64.b64encode(file_as_bytes).decode('ascii')
 
         harmony_file = RawFile(file_type=file_type, content="," + file_as_base64, file_id=uuid.uuid4().hex,
-                               instrument_name=file_name, file_name=file_name)
+                               file_name=file_name)
     else:
         with open(
                 file_name,
                 "r", encoding="utf-8") as f:
             file_as_string = f.read()
         harmony_file = RawFile(file_type="txt", content=file_as_string, file_id=uuid.uuid4().hex,
-                               instrument_name=file_name, file_name=file_name)
+                               file_name=file_name)
 
     return convert_files_to_instruments([harmony_file])
