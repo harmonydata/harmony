a393119ee80b7dd070295b4f67c40161bcdf09e1
Eve <evecheng999@gmail.com>
2023-12-23 18:18:45 +1100

    Add wmd_match and testing

---
+++ src/harmony/matching/wmd_matcher.py
@@ -0,0 +1,40 @@
+from wmd import WMD
+import numpy as np
+import math
+
+def euclidean_dist(point1, point2):
+    if len(point1) != len(point2):
+        raise ValueError("Points must have the same number of dimensions")
+
+    squared_distance = sum((p1 - p2) ** 2 for p1, p2 in zip(point1, point2))
+    distance = math.sqrt(squared_distance)
+    return distance
+
+def par_to_vecs(par,vectorisation_function):
+    return [vectorisation_function(sent) for sent in par]
+
+def dist(vecs1,vecs2):
+    vec_union = list(vecs1 + vecs2)
+    n1,n2 = len(vecs1),len(vecs2)
+    n = len(vec_union)
+    dist_ = np.zeros((n,n))
+    for i in range(n):
+        for j in range(i):
+            dist_[i,j] = dist_[j,i] = euclidean_dist(vec_union[i],vec_union[j])
+
+    nw1 = [1 for i in range(n1)]+[0 for i in range(n2)]
+    nw2 = [1 for i in range(n2)]+[0 for i in range(n1)]
+    return dist_,nw1,nw2
+
+
+def pars_dist_emd_emdrelaxed(par1,par2,vectorisation_function):
+    relax_cache = libwmdrelax.emd_relaxed_cache_init(1e5) 
+    cache = libwmdrelax.emd_cache_init(1e5) 
+ 
+    vecs1,vecs2 = par_to_vecs(par1),par_to_vecs(par2)
+    dist_,nw1,nw2 = dist(vecs1,vecs2)
+    emd = libwmdrelax.emd(nw1,nw2,dist_,)
+    emd_relaxed = libwmrelax.emd_relaxed(nw1,nw2,dist_,relax_cache)
+
+
+    
