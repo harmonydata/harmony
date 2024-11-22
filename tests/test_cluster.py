0eb4dc9857233047c435e143a24f844174230c20
vkrithika25 <v.krithika@gmail.com>
2024-11-22 14:41:09 -0500

    Added clustering functionality with optional graphing in cluster.py, with associated tests in test_cluster.py

---
+++ tests/test_cluster.py
@@ -0,0 +1,51 @@
+'''
+MIT License
+
+Copyright (c) 2023 Ulster University (https://www.ulster.ac.uk).
+Project: Harmony (https://harmonydata.ac.uk)
+Maintainer: Thomas Wood (https://fastdatascience.com)
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+'''
+
+import sys
+import unittest
+
+sys.path.append("../src")
+
+from harmony.matching.cluster import cluster_questions
+from harmony import create_instrument_from_list, import_instrument_into_harmony_web
+from harmony.schemas.requests.text import Instrument, Question
+
+
+class TestCluster(unittest.TestCase):
+    def setUp(self):
+        self. all_questions_real = [Question(question_no="1", question_text="Feeling nervous, anxious, or on edge"),
+                              Question(question_no="2", question_text="Not being able to stop or control worrying"),
+                              Question(question_no="3", question_text="Little interest or pleasure in doing things"),
+                              Question(question_no="4", question_text="Feeling down, depressed, or hopeless"),
+                              Question(question_no="5",
+                                       question_text="Trouble falling/staying asleep, sleeping too much"), ]
+        self.instruments = Instrument(questions=self.all_questions_real)
+
+    def test_cluster(self):
+        clusters_out, score_out = cluster_questions(self.instruments, 2, True)
+        assert(len(clusters_out) == 5)
+        assert score_out
