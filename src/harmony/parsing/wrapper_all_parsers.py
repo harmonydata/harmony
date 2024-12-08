12e1f9520024f6d920624e04c49039c0ff702654
AliceeLe <anhle@andrew.cmu.edu>
2024-12-08 01:55:10 -0500

    Revert back unnecessary changes

--- src/harmony/parsing/wrapper_all_parsers.py
+++ src/harmony/parsing/wrapper_all_parsers.py
@@ -30,7 +30,6 @@ from typing import List
 from harmony.parsing.excel_parser import convert_excel_to_instruments
 from harmony.parsing.pdf_parser import convert_pdf_to_instruments
 from harmony.parsing.text_parser import convert_text_to_instruments
-# from harmony.parsing.html_parser import convert_html_to_instruments
 from harmony.schemas.enums.file_types import FileType
 from harmony.schemas.requests.text import RawFile, Instrument
 
@@ -42,8 +41,6 @@ def _get_instruments_from_file(file):
         instruments_from_this_file = convert_text_to_instruments(file)
     elif file.file_type == FileType.xlsx:
         instruments_from_this_file = convert_excel_to_instruments(file)
-    # elif file.file_type == FileType.html:
-    #     instruments_from_this_file = convert_html_to_instruments(file)
     else:
         instruments_from_this_file = []
     return instruments_from_this_file
