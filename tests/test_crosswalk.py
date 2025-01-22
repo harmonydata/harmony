e37c7fa621e5cf8505e0cce00987fd3ae51dd805
Thomas Wood <thomas@fastdatascience.com>
2025-01-22 12:25:19 +0000

    Crosswalk now takes instruments as input and allows user to enforce 1:1 matching or allow within instrument matches

--- tests/test_crosswalk.py
+++ tests/test_crosswalk.py
@@ -27,63 +27,65 @@ SOFTWARE.
 
 import sys
 import unittest
-import pandas as pd
+
 import numpy as np
+import pandas as pd
 
 sys.path.append("../src")
 
 from harmony.matching.generate_crosswalk_table import generate_crosswalk_table
+from harmony import create_instrument_from_list
 from harmony import match_instruments
-from harmony.schemas.requests.text import Instrument, Question
+
 
 class TestGenerateCrosswalkTable(unittest.TestCase):
     def setUp(self):
         # Sample data
-        self.all_questions_dummy = [
-            Question(question_no="1", question_text="potato"),
-            Question(question_no="2", question_text="tomato"),
-            Question(question_no="3", question_text="radish"),
-        ]
-
-        self.instruments_dummy = Instrument(questions=self.all_questions_dummy)
+        self.instruments_dummy = [create_instrument_from_list(["potato", "tomato", "radish"], instrument_name="veg")]
 
         self.similarity = np.array([
             [1.0, 0.7, 0.9],
             [0.7, 1.0, 0.8],
             [0.9, 0.8, 1.0]
         ])
-        self.all_questions_real = [Question(question_no="1", question_text="Feeling nervous, anxious, or on edge"),
-                        Question(question_no="2", question_text="Not being able to stop or control worrying")]
-        self.instruments = Instrument(questions=self.all_questions_real)
+
+        self.instruments = [create_instrument_from_list(
+            ["Feeling nervous, anxious, or on edge", "Not being able to stop or control worrying"],
+            instrument_name="GAD-7")]
 
         self.threshold = 0.6
 
-
     def test_generate_crosswalk_table_dummy_data(self):
-        result = generate_crosswalk_table(self.instruments_dummy.questions, self.similarity, self.threshold)
+        result = generate_crosswalk_table(self.instruments_dummy, self.similarity, self.threshold,
+                                          is_allow_within_instrument_matches=True)
 
         expected_matches = [
-            {"pair_name": "0_1", "question1_no": "1", "question1_text": "potato",
-             "question2_no": "2", "question2_text": "tomato", "match_score": 0.7},
-            {"pair_name": "0_2", "question1_no": "1", "question1_text": "potato",
-             "question2_no": "3", "question2_text": "radish", "match_score": 0.9},
-            {"pair_name": "1_2", "question1_no": "2", "question1_text": "tomato",
-             "question2_no": "3", "question2_text": "radish", "match_score": 0.8},
-        ]
-
-        for _, row in pd.DataFrame(expected_matches).iterrows():
-            self.assertTrue(any(row.equals(result_row) for _, result_row in result.iterrows()))
+            {'match_score': 0.9, 'pair_name': 'veg_1_veg_3', 'question1_id': 'veg_1', 'question1_text': 'potato',
+             'question2_id': 'veg_3', 'question2_text': 'radish'},
+            {'match_score': 0.8, 'pair_name': 'veg_2_veg_3', 'question1_id': 'veg_2', 'question1_text': 'tomato',
+             'question2_id': 'veg_3', 'question2_text': 'radish'},
+            {'match_score': 0.7, 'pair_name': 'veg_1_veg_2', 'question1_id': 'veg_1', 'question1_text': 'potato',
+             'question2_id': 'veg_2', 'question2_text': 'tomato'}]
 
         self.assertEqual(len(result), len(expected_matches))
 
+        for row_idx, expected_row in enumerate(expected_matches):
+            self.assertEqual(expected_row["match_score"], result["match_score"].iloc[row_idx])
+            self.assertEqual(expected_row["pair_name"], result["pair_name"].iloc[row_idx])
+            self.assertEqual(expected_row["question1_id"], result["question1_id"].iloc[row_idx])
+            self.assertEqual(expected_row["question2_id"], result["question2_id"].iloc[row_idx])
+            self.assertEqual(expected_row["question1_text"], result["question1_text"].iloc[row_idx])
+            self.assertEqual(expected_row["question2_text"], result["question2_text"].iloc[row_idx])
+
     def test_generate_crosswalk_table_empty(self):
         empty_similarity = np.eye(3)  # Identity matrix, no matches above threshold
-        result = generate_crosswalk_table(self.all_questions_dummy, empty_similarity, self.threshold)
+        result = generate_crosswalk_table(self.instruments_dummy, empty_similarity, self.threshold)
         self.assertTrue(result.empty)
 
     def test_generate_crosswalk_table_real(self):
-        all_questions, similarity_with_polarity, _, _ = match_instruments([self.instruments])
-        result = generate_crosswalk_table(all_questions, similarity_with_polarity, self.threshold)
+        all_questions, similarity_with_polarity, _, _ = match_instruments(self.instruments)
+        result = generate_crosswalk_table(self.instruments, similarity_with_polarity, self.threshold,
+                                          is_allow_within_instrument_matches=True)
         expected_matches = []
 
         for _, row in pd.DataFrame(expected_matches).iterrows():
@@ -92,9 +94,34 @@ class TestGenerateCrosswalkTable(unittest.TestCase):
         self.assertEqual(len(result), len(expected_matches))
 
         lower_threshold = 0.5
-        result = generate_crosswalk_table(all_questions, similarity_with_polarity, lower_threshold)
+        result = generate_crosswalk_table(self.instruments, similarity_with_polarity, lower_threshold,
+                                          is_allow_within_instrument_matches=True)
 
         self.assertEqual(len(result), 1)
+
+    def test_crosswalk_two_instruments_allow_many_to_one_matches(self):
+
+        instrument_1 = create_instrument_from_list(["I felt fearful."])
+        instrument_2 = create_instrument_from_list(
+            ["Feeling afraid, as if something awful might happen", "Feeling nervous, anxious, or on edge"])
+        instruments = [instrument_1, instrument_2]
+
+        all_questions, similarity_with_polarity, _, _ = match_instruments(instruments)
+        result = generate_crosswalk_table(instruments, similarity_with_polarity, 0, is_enforce_one_to_one=False)
+
+        self.assertEqual(2, len(result))
+
+    def test_crosswalk_two_instruments_enforce_one_to_one_matches(self):
+
+        instrument_1 = create_instrument_from_list(["I felt fearful."])
+        instrument_2 = create_instrument_from_list(
+            ["Feeling afraid, as if something awful might happen", "Feeling nervous, anxious, or on edge"])
+        instruments = [instrument_1, instrument_2]
+
+        all_questions, similarity_with_polarity, _, _ = match_instruments(instruments)
+        result = generate_crosswalk_table(instruments, similarity_with_polarity, 0, is_enforce_one_to_one=True)
+
+        self.assertEqual(1, len(result))
 
 
 if __name__ == '__main__':
