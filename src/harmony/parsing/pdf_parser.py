daf56377b9721f2cae3492daf431da0f3788eb33
Monty Gole <montygole@gmail.com>
2025-05-01 16:36:56 -0400

    Refactor predict() and convert_pdf_to_instruments() to handle fine-tuned LLM parser

--- src/harmony/parsing/pdf_parser.py
+++ src/harmony/parsing/pdf_parser.py
@@ -29,53 +29,80 @@ import pathlib
 import pickle as pkl
 import re
 
+from torch.cuda import device_of
+
 import harmony
 from harmony.parsing.util.feature_extraction import convert_text_to_features
 from harmony.parsing.util.tika_wrapper import parse_pdf_to_plain_text
 from harmony.schemas.requests.text import RawFile, Instrument
+from transformers import AutoModelForTokenClassification, AutoTokenizer
+import torch
 
-model_containing_folder = pathlib.Path(__file__).parent.resolve()
+def group_token_spans_by_class(tokens, classes, tokenizer) -> dict:
+    """
+    Given a list of tokens, and a list of predicted classes
+    for each token, create a dictionary to hold each
+    span of tokens.
+    Example:
+        > group_token_spans_by_classes(['How', 'are', 'you', '?', 'Alright'],
+                                        ['question', 'question', 'question', 'question', 'answer'],
+                                        bert_tokenizer)
+        > {"question":["How are you?"], "answer":["Alright"]}
 
-with open(f"{model_containing_folder}/20240719_pdf_question_extraction_sklearn_crf_model.pkl", "rb") as f:
-    crf_text_model = pkl.load(f)
+    :param tokens: List of tokens
+    :type tokens: List[str]
+    :param classes: List of predicted classes
+    :type classes: List[str]
+    :param tokenizer: Tokenizer
+    :return: Dictionary of each span relative to its class
+    """
+    grouped_spans = {"answer":[], "question":[], "other":[]}
+    span = []
+    prev_cls = None
 
-# Predict method is taken from the training repo. Use the training repo as the master copy of the predict method.
-# All training code is in https://github.com/harmonydata/pdf-questionnaire-extraction
+    for token, cls in zip(tokens, classes):
+        if cls != prev_cls and span:
+            grouped_spans[prev_cls].append(tokenizer.convert_tokens_to_string(span))
+            span = []
+        span.append(token)
+        prev_cls = cls
+    # Add final token and class to respective key in dictionary
+    if span:
+        grouped_spans[prev_cls].append(tokenizer.convert_tokens_to_string(span))
+
+    return grouped_spans
+
+
 def predict(test_text):
-    token_texts, token_start_char_indices, token_end_char_indices, token_properties = convert_text_to_features(
-        test_text)
+    # Load fine-tuned huggingface model and tokenizer
+    model = AutoModelForTokenClassification.from_pretrained("harmonydata/debertaV2_pdfparser")
+    tokenizer = AutoTokenizer.from_pretrained("harmonydata/debertaV2_pdfparser")
 
-    X = []
-    X.append(token_properties)
+    # Tokenize input text
+    tokenized_texts = tokenizer(test_text, return_tensors="pt")
 
-    y_pred = crf_text_model.predict(X)
+    # Inference with tokenized input text
+    with torch.no_grad():
+        logits = model(**tokenized_texts).logits
 
-    questions_from_text = []
+    # Retrieve predicted class for each token
+    predictions = torch.argmax(logits, dim=2)
+    predicted_token_class = [model.config.id2label[t.item()] for t in predictions[0]]
 
-    tokens_already_used = set()
+    # Get input IDs (tensor) and convert to list
+    input_ids = tokenized_texts["input_ids"][0].tolist()
+    # Convert input IDs to tokens
+    decoded_tokenized_texts = tokenizer.convert_ids_to_tokens(input_ids)
 
-    last_token_category = "O"
+    # Remove leading [CLS] and trailing [SEP] tokens from decoded
+    # tokens, and the list of predictions
+    predicted_token_class = predicted_token_class[1:-1]
+    decoded_tokenized_texts = decoded_tokenized_texts[1:-1]
 
-    for idx in range(len(X[0])):
+    grouped_tokens = group_token_spans_by_class(decoded_tokenized_texts, predicted_token_class, tokenizer)
 
-        if y_pred[0][idx] != "O" and idx not in tokens_already_used:
-            if last_token_category == "O" or y_pred[0][idx] == "B":
-                start_idx = token_start_char_indices[idx]
-                end_idx = len(test_text)
-                for j in range(idx + 1, len(X[0])):
-                    if y_pred[0][j] == "O" or y_pred[0][j] == "B":
-                        end_idx = token_end_char_indices[j - 1]
-                        break
-                    tokens_already_used.add(j)
+    return grouped_tokens
 
-                question_text = test_text[start_idx:end_idx]
-                question_text = re.sub(r'\s+', ' ', question_text)
-                question_text = question_text.strip()
-                questions_from_text.append(question_text)
-
-        last_token_category = y_pred[0][idx]
-
-    return questions_from_text
 
 
 def convert_pdf_to_instruments(file: RawFile) -> Instrument:
@@ -87,7 +114,7 @@ def convert_pdf_to_instruments(file: RawFile) -> Instrument:
     if not file.text_content:
         file.text_content = parse_pdf_to_plain_text(file.content)  # call Tika to convert the PDF to plain text
 
-    questions_from_text = predict(file.text_content)
+    questions_from_text = predict(file.text_content)["question"]
 
     instrument = harmony.create_instrument_from_list(questions_from_text, instrument_name=file.file_name,
                                                      file_name=file.file_name)
